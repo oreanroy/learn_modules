9e1678e4c843429303f767b9de70b51e0033d9bc
oreanroy <oreanroy@gmail.com>
2019-07-02 16:42:59 +0530

    The mutitple file operations

---
+++ lpyHardway/funcions/ex7.py
@@ -0,0 +1,38 @@
+def break_words(stuff):
+	words = stuff.split(' ')
+	return words
+
+def sort_words(words):
+	return sorted(words)
+
+def print_first_word(words):
+	word = words.pop(0)
+	print word
+
+def print_last_word(words):
+	word = words.pop(-1)
+	print word
+
+def sort_sentence(sentence):
+	words = break_words(sentence)
+	return sort_words(words)
+
+def print_first_and_last(sentence):
+	"""prints the first and last word of sentence"""
+	words = break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+	"""prints the first and last words of the sentence"""
+	words = sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+	"""Sorts the words then prints the first and last one."""
+	words = sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+
