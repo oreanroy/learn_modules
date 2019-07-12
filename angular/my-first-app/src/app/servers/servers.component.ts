fd06cfff6e9107a160c620a15c6b36a1fdb5811a
oreanroy <oreanroy@gmail.com>
2019-07-12 15:46:24 +0530

    started with directives

--- angular/my-first-app/src/app/servers/servers.component.ts
+++ angular/my-first-app/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'testServer';
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = "Server was created! Name is. "+ this.serverName
   }
 
